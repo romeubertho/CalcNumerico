4c97c91320c367da733105f3fe11a6bc32abfe04
root <romeubertho@gmail.com>
2018-07-03 00:16:59 -0300

    Add questao

---
+++ T2/Questao.m
@@ -0,0 +1,12 @@
+function [ Root ] = Questao(f,x0, eps)
+%QUESTAO Summary of this function goes here
+%   Detailed explanation goes here
+F1=CotesSimpson13(@(x)(1/sqrt(2*pi)*exp((-x.^2)/2)), 0, 1, 10e-10)-0.45;
+F2=CotesSimpson13(@(x)(1/sqrt(2*pi)*exp((-x.^2)/2)), 0, 2, 10e-10)-0.45;
+fprintf('\nF(1) * F(2) = %f\n\n',F1*F2);
+
+Root=metodo_newton(f,x0,eps);
+
+end
+
+
