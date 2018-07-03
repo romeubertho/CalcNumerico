11f050c1a536192b18b819253cec6a18b9fc0976
root <romeubertho@gmail.com>
2018-07-02 23:11:09 -0300

    add call CotesSimpson13

--- T2/metodo_newton.m
+++ T2/metodo_newton.m
@@ -1,16 +1,16 @@
 function out = metodo_newton(f,x0, eps)
-    
-    delta_derivada = exp(-10);   
+      
     delta = abs(x0);
     xn = x0;
-
+    
     while delta > eps    
-        %derivada_em_xn = f(xn)
-        %x_n_mais_1 = xn-simpson(@(x))/derivada_em_xn
-        derivada_em_xn = (f(xn+delta_derivada)-f(xn))/delta_derivada;
-        x_n_mais_1 = xn - (f(xn)/derivada_em_xn);
-        delta = abs(x_n_mais_1-xn);
-        xn = x_n_mais_1;        
+        derivada_em_xn = f(xn);
+        primitiva=CotesSimpson13(f,0,xn,1,eps,0);
+        x_n_mais_1 = xn-(primitiva-0.45)/derivada_em_xn;
+        %derivada_em_xn = (f(xn+delta_derivada)-f(xn))/delta_derivada;
+        %x_n_mais_1 = xn - (f(xn)/derivada_em_xn);
+        delta = abs(x_n_mais_1-xn)
+        xn = x_n_mais_1        
     end
     out = xn
     
